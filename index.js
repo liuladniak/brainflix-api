86305839f73b9da0003fe89a5d61f78259340bcf
Liu Ladniak <liuladniak@gmail.com>
2024-06-17 22:11:28 -0400

    setup, adds npm packages

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from "express";
+import videoRoutes from "./routes/videos.js";
 import cors from "cors";
-import crypto from "crypto";
 import "dotenv/config";
-import fs from "fs";
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
 // Home route
 app.get("/", (req, res) => {
-  res.send("Welcome to application");
+  res.send("Homepage");
 });
 
 app.listen(PORT, () => {
--- routes/videos.js
+++ routes/videos.js
@@ -1,1 +1,5 @@
+import express from "express";
+import fs from "fs";
+import crypto from "crypto";
 
+const router = express.Router();
