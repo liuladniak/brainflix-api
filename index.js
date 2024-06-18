4e780c2e5f0acecccb83ed9c63dd8ec0e9534738
Liu Ladniak <liuladniak@gmail.com>
2024-06-18 14:33:04 -0400

    adds get and post videos routes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
-import videoRoutes from "./routes/videos.js";
+import videosRouter from "./routes/videos.js";
 import cors from "cors";
 import "dotenv/config";
+import fs from "fs";
 
 const app = express();
 
@@ -9,12 +10,16 @@ app.use(cors());
 
 app.use(express.json());
 
+// app.use("/images", express.static("./public/images"));
+
 const PORT = process.env.PORT || 8080;
 
 // Home route
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
+
+app.use("/videos", videosRouter);
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
--- routes/videos.js
+++ routes/videos.js
@@ -3,3 +3,60 @@ import fs from "fs";
 import crypto from "crypto";
 
 const router = express.Router();
+
+const readVideoFile = () => {
+  const videoFile = fs.readFileSync("./data/videos.json");
+  const videoData = JSON.parse(videoFile);
+
+  return videoData;
+};
+
+const writeVideoFile = (data) => {
+  const strigifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/videos.json", strigifiedData);
+};
+
+router.get("/", (req, res) => {
+  const videoData = readVideoFile();
+
+  const strippedData = videoData.map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+    };
+  });
+  res.json(strippedData);
+});
+
+router.get("/:id", (req, res) => {
+  const videoData = readVideoFile();
+
+  const singleVideo = videoData.find((video) => video.id === req.params.id);
+  if (singleVideo) {
+    res.json(singleVideo);
+  } else {
+    res.status(404).json({
+      message: "Video not found",
+      error: "404",
+    });
+  }
+});
+
+router.post("/", (req, res) => {
+  const newVideo = {
+    id: crypto.randomUUID(),
+    title: req.body.title,
+    channel: req.body.channel,
+    image: req.body.image,
+  };
+
+  const videoData = readVideoFile();
+
+  videoData.push(newVideo);
+  writeVideoFile(videoData);
+
+  res.status(201).json(newVideo);
+});
+export default router;
